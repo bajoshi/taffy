33c472e85141ac2db947d1f3642079cce38d3042
Bhavin Joshi <bajoshi@asu.edu>
2017-07-06 12:45:50 -0700

    ew_hbeta code updated -- still needs testing

--- codes/ew_hbeta.py
+++ codes/ew_hbeta.py
@@ -18,25 +18,53 @@ taffy_extdir = '/Volumes/Bhavins_backup/ipac/TAFFY/'
 
 import bpt_plots as bpt
 
-if __name__ == '__main__':
+def test_pix(blue_cont):
 
-    # read in lzifu output file
-    h = fits.open(taffy_extdir + 'big_cube_2_comp_velsort.fits')
+    pix_x = 18
+    pix_y = 39
+    arr_x = pix_y - 1
+    arr_y = pix_x - 1
 
-    blue_cont = h['B_CONTINUUM'].data
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
 
-    # mask elements where LZIFU gave NaNs
-    region_file = open(taffy_extdir + 'vel_comp.reg')
-    region_list = np.array(region_file.readlines()[-1].split('(')[1].split(')')[0].split(','))
-    region_list = region_list.astype(np.float64)
-    region_file.close()
+    ax.plot(np.arange(len(blue_cont[:,arr_x,arr_y])), blue_cont[:,arr_x,arr_y], color='k')
+    plt.show()
+
+    return None
+
+def get_pn(region_list):
 
     pn_list = []
     for i in range(0,len(region_list),2):
         pn_list.append([int(round(region_list[i])),int(round(region_list[i+1]))])
 
-    region_pn = pg.Polygon(pn_list)
-    all_mask = bpt.getregionmask(region_pn, (58,58), "region.")
+    return pg.Polygon(pn_list)
+
+if __name__ == '__main__':
+
+    # read in lzifu output file
+    h = fits.open(taffy_extdir + 'products_big_cube_velsort/big_cube_2_comp_velsort.fits')
+
+    blue_cont = h['B_CONTINUUM'].data
+
+    # mask elements where LZIFU gave NaNs
+    region_file = open(taffy_extdir + 'NS_2017new_xy.reg')
+
+    for line in region_file.readlines()[3:]:
+
+        line_reg = line.split('(')[1].split(')')[1].split(' ')[-1].rstrip()
+        region_list = np.array(line.split('(')[1].split(')')[0].split(','))
+
+        if line_reg == 'north':
+            north_pn = get_pn(region_list.astype(np.float64))
+            north_mask = bpt.getregionmask(north_pn, (58,58), "North galaxy region.")
+
+        elif line_reg == 'south':
+            south_pn = get_pn(region_list.astype(np.float64))
+            south_mask = bpt.getregionmask(south_pn, (58,58), "South galaxy region.")
+
+    region_file.close()
 
     # create wavelength array
     # I read these data from the header
@@ -61,17 +89,20 @@ if __name__ == '__main__':
     for i in range(0,58):
         for j in range(0,58):
 
+            pix_x = j + 1
+            pix_y = i + 1
+
+            #print "On ds9 pixel (x,y):", pix_x, pix_y
+
             # checks to see that the pixel is not masked and 
             # that no elements in the fitting array are NaN
-            if all_mask[i,j]:
-                ew_map[i,j] = np.nan
-                continue
+            #if all_mask[i,j]:
+            #    ew_map[i,j] = np.nan
+            #    continue
 
             if all(np.isnan(blue_cont[:,i,j])):
                 ew_map[i,j] = np.nan
                 continue
-
-
 
             # initialize fitting arrays
             left_arr = blue_cont[hbeta_idx-175:hbeta_idx-125, i , j]
@@ -80,11 +111,6 @@ if __name__ == '__main__':
 
             blue_cont_fit_yarr = blue_cont[hbeta_idx-500:hbeta_idx+500, i , j]
             blue_cont_fit_xarr = np.linspace(hbeta_idx-500, hbeta_idx+500, len(blue_cont_fit_yarr))
-
-            # check that none of the fitting array elements is NaN
-            #if any(np.isnan(blue_cont_fit_yarr)):
-            #    ew_map[i,j] = np.nan
-            #    continue
 
             # normalize the y array by the continuum to get a continuum level of approx. 1
             blue_cont_fit_yarr_norm = blue_cont_fit_yarr / cont_mean
@@ -122,6 +148,7 @@ if __name__ == '__main__':
             fig = plt.figure()
             ax = fig.add_subplot(111)
 
+            ax.plot(np.arange(len(blue_cont[:,i,j])), blue_cont[:,i,j], color='k')
             ax.plot(blue_cont_fit_xarr, blue_cont_fit_yarr_norm, lw=2)
             ax.plot(blue_cont_fit_xarr, g(blue_cont_fit_xarr), ls='--', color='g', lw=2)
             ax.plot(blue_cont_fit_xarr, l(blue_cont_fit_xarr), ls='--', color='r', lw=2)
@@ -141,17 +168,28 @@ if __name__ == '__main__':
                     abs_area = a2 - a1
 
                     abs_area_analytic = comb_amp * comb_stddev * np.sqrt(2 * np.pi)
-                    #print i,j, cont_mean_norm, abs_area, abs_area_analytic
 
             else:
                 abs_area_analytic = 0.0
 
-            ew_map[i,j] = abs_area_analytic / cont_mean
+            ew_map[i,j] = abs_area_analytic #/ cont_mean
+
+            #print abs_area_analytic, abs_area
+            #print cont_mean
+            #print ew_map[i,j]
+            #sys.exit(0)
 
     #ew_map = ma.array(ew_map, mask=all_mask)
 
-    plt.imshow(ew_map, vmin=0, vmax=5, cmap='viridis', origin='lower')
+    ew_map_north = ma.array(ew_map, mask=north_mask)
+    ew_map_south = ma.array(ew_map, mask=south_mask)
+
+    plt.imshow(ew_map_north, cmap='viridis', origin='lower')
     plt.colorbar()
     plt.show()
-    
+
+    plt.imshow(ew_map_south, cmap='viridis', origin='lower')
+    plt.colorbar()
+    plt.show()
+
     sys.exit(0)
