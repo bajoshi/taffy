1718e7af1dedfd1ceb7197f8efd021606fe80890
Bhavin Joshi <bajoshi@asu.edu>
2018-06-20 12:41:38 -0500

    Adding significance cut on taffy dust calculation

--- codes_stitched_cube/taffy_dust_map.py
+++ codes_stitched_cube/taffy_dust_map.py
@@ -18,6 +18,10 @@ sys.path.append(taffydir + 'codes/')
 import vel_channel_map as vcm
 
 if __name__ == '__main__':
+    """
+    TO-DO:
+    1. Dust correction on spaxel-by-spaxel basis
+    """
 
     # get mask of all possible not NaN pixels
     all_mask = vcm.get_region_mask('all_possibly_notnan_pixels_new')
@@ -25,9 +29,47 @@ if __name__ == '__main__':
     # read in halpha and hbeta maps from stitched cube
     halpha = fits.open(taffy_extdir + 'stitched_cube_HALPHA.fits')
     hbeta = fits.open(taffy_extdir + 'stitched_cube_HBETA.fits')
+    halpha_err = fits.open(taffy_extdir + 'stitched_cube_HALPHA_ERR.fits')
+    hbeta_err = fits.open(taffy_extdir + 'stitched_cube_HBETA_ERR.fits')
+
+    # assign to 2D numpy arrays
     halpha_total = halpha[0].data[0]
     hbeta_total = hbeta[0].data[0]
 
+    halpha_total_err = halpha_err[0].data[0]
+    hbeta_total_err = hbeta_err[0].data[0]
+
+    # make snr maps
+    halpha_snr = halpha_total / halpha_total_err
+    hbeta_snr = hbeta_total / hbeta_total_err
+
+    plt.imshow(halpha_snr, origin='lower', interpolation='None')
+    plt.show()
+    plt.imshow(hbeta_snr, origin='lower', interpolation='None')
+    plt.show()
+
+    # find all spaxels iwth significant measurements for both lines
+    val_idx1 = np.where(halpha_snr >= 3.0)
+    val_idx2 = np.where(hbeta_snr >= 3.0)
+
+    halpha_snrmask = np.ones(halpha_total.shape, dtype=bool)
+    hbeta_snrmask = np.ones(hbeta_total.shape, dtype=bool)
+    halpha_snrmask[val_idx1[0], val_idx1[1]] = False
+    hbeta_snrmask[val_idx2[0], val_idx2[1]] = False
+    val_idx = ma.mask_or(halpha_snrmask, hbeta_snrmask)
+
+    # inverse mask to show only valid elements
+    halpha_snr = ma.array(halpha_snr, mask=np.logical_not(val_idx))
+    hbeta_snr = ma.array(hbeta_snr, mask=np.logical_not(val_idx))
+
+    plt.imshow(halpha_snr, origin='lower', interpolation='None')
+    plt.show()
+    plt.imshow(hbeta_snr, origin='lower', interpolation='None')
+    plt.show()
+
+    sys.exit(0)
+
+    # Now find the ratio and get dust extinction
     # this assumes that all the halpha and hbeta comes from
     # case B which is not the case. We are explicitly saying 
     # htat some of the ionized gas emission comes from shocks.
@@ -35,7 +77,11 @@ if __name__ == '__main__':
     # the fractions coming from shocks and from SF separately.
 
     # loop over all pixels and get a ebv value for each
+
     # ebv[i,j] = 1.97 * np.log10(halpha[i,j]/hbeta[i,j] / 2.86)
+    # this formula with the constant 1.97 out front came from Phil
+    # I'm not sure of that constant. So I'm usign the formula I derived for now.
+
     ebv_map = np.zeros((58,58))
 
     for i in range(58):
@@ -44,7 +90,7 @@ if __name__ == '__main__':
             ebv_map[i,j] = 2.37 * np.log10(halpha_total[i,j]/hbeta_total[i,j] / 2.85)
 
     # apply mask
-    ebv_map = ma.array(ebv_map, mask=all_mask)
+    #ebv_map = ma.array(ebv_map, mask=all_mask)
 
     # figure
     print np.nanmin(4.05*ebv_map)
