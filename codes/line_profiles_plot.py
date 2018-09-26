e382f4b195c3cf29f3b21e5a62631ddba453a859
Bhavin Joshi <bajoshi@asu.edu>
2018-09-26 14:21:45 -0700

    Many updates to plotting line profiles

--- codes/line_profiles_plot.py
+++ codes/line_profiles_plot.py
@@ -49,18 +49,20 @@ def plot_line_profiles(axesblue, axesred, bluewav, bluespec, redwav, redspec, \
     # do not show the spines except at the bottom
     axesblue.spines["top"].set_visible(False)
     axesblue.spines["right"].set_visible(False)
-    axesblue.spines["left"].set_visible(False)
+    axesblue.spines["left"].set_visible(True)
 
     axesred.spines["top"].set_visible(False)
     axesred.spines["right"].set_visible(False)
-    axesred.spines["left"].set_visible(False)
+    axesred.spines["left"].set_visible(True)
 
     # do not show the ticks
-    axesblue.get_yaxis().set_ticklabels([])
-    axesblue.get_yaxis().set_ticks([])
+    #axesblue.get_yaxis().set_ticklabels([])
+    #axesblue.get_yaxis().set_ticks([])
+    #axesred.get_yaxis().set_ticklabels([])
+    #axesred.get_yaxis().set_ticks([])
 
-    axesred.get_yaxis().set_ticklabels([])
-    axesred.get_yaxis().set_ticks([])
+    axesblue.tick_params(axis='y', which='both', right=False)
+    axesred.tick_params(axis='y', which='both', right=False)
 
     # for now set xticklabels to empty # it will be edited by hand in __main__
     axesblue.get_xaxis().set_ticklabels([])
@@ -206,6 +208,29 @@ if __name__ == '__main__':
 
     ax.coords.frame.set_color('None')
 
+    # Add polygon patches for galaxies and bridge
+    north_poly_points = [[0.422200173,23.50396718],[0.4152304334,23.50387652],[0.4155502808,23.50226454],\
+    [0.414591942,23.49908922],[0.4180543574,23.49850344],[0.4199720955,23.49723355],[0.4259616693,23.49127482],\
+    [0.4286967795,23.48904169],[0.4291162163,23.49004197],[0.4313178761,23.49288664],[0.4291871345,23.49689221]]
+    south_poly_points = [[0.4071288157,23.49766402],[0.4027494899,23.49619723],[0.4031346441,23.48922165],\
+    [0.4060123993,23.48189481],[0.4078664731,23.47872971],[0.410943916,23.47614935],[0.4158554494,23.47630534],\
+    [0.4202012426,23.47791063],[0.4217348365,23.48048999],[0.4213999114,23.48310946],[0.417944079,23.48275696],\
+    [0.4133603491,23.48707351],[0.4114718625,23.48932533],[0.4090425678,23.49400985]]
+    bridge_poly_points = [[0.4071288316,23.49758635],[0.4090459205,23.49391271],[0.4114969024,23.48922357],\
+    [0.4133614547,23.48697676],[0.4179534303,23.48268663],[0.421404655,23.48303866],[0.4251755324,23.48233549],\
+    [0.4271566586,23.48509069],[0.4286953339,23.48900495],[0.4258300899,23.49137329],[0.4199687248,23.49723481],\
+    [0.4180519391,23.49850339],[0.412549028,23.49960668]]
+
+    pn = Polygon(np.array(north_poly_points), edgecolor='k', facecolor='None', \
+        closed=True, transform=ax.get_transform('fk5'), lw=1.5)
+    ax.add_patch(pn)
+    ps = Polygon(np.array(south_poly_points), edgecolor='k', facecolor='None', \
+        closed=True, transform=ax.get_transform('fk5'), lw=1.5)
+    ax.add_patch(ps)
+    pb = Polygon(np.array(bridge_poly_points), edgecolor='k', facecolor='None', \
+        closed=True, transform=ax.get_transform('fk5'), lw=1.5)
+    ax.add_patch(pb)
+
     # Add text to figure to indicate region name
     f = FontProperties()
     f.set_weight('bold')
@@ -223,7 +248,7 @@ if __name__ == '__main__':
 
     ax.text(0.38, 0.63, 'B1', verticalalignment='top', horizontalalignment='left', \
         transform=ax.transAxes, color='k', fontproperties=f, size=12)
-    ax.text(0.52, 0.81, 'B2', verticalalignment='top', horizontalalignment='left', \
+    ax.text(0.54, 0.79, 'B2', verticalalignment='top', horizontalalignment='left', \
         transform=ax.transAxes, color='k', fontproperties=f, size=12)
     ax.text(0.47, 0.52, 'B3', verticalalignment='top', horizontalalignment='left', \
         transform=ax.transAxes, color='k', fontproperties=f, size=12)
@@ -234,15 +259,28 @@ if __name__ == '__main__':
         transform=ax.transAxes, color='k', fontproperties=f, size=12)
     ax.text(0.71, 0.49, 'S3', verticalalignment='top', horizontalalignment='left', \
         transform=ax.transAxes, color='k', fontproperties=f, size=12)
-    ax.text(0.75, 0.56, 'S4', verticalalignment='top', horizontalalignment='left', \
+    ax.text(0.74, 0.6, 'S4', verticalalignment='top', horizontalalignment='left', \
         transform=ax.transAxes, color='k', fontproperties=f, size=12)
-    ax.text(0.75, 0.64, 'S5', verticalalignment='top', horizontalalignment='left', \
+    ax.text(0.73, 0.68, 'S5', verticalalignment='top', horizontalalignment='left', \
         transform=ax.transAxes, color='k', fontproperties=f, size=12)
 
-    ax.text(0.05, 0.1, r'$\mathrm{[OIII]\lambda 5007}$', verticalalignment='top', horizontalalignment='left', \
+    ax.text(0.02, 0.1, r'$\mathrm{[OIII]\lambda 5007}$' + '  ' + r'$\mathrm{Velocity\ scale\, (km/s):[-575,+623]}$', \
+        verticalalignment='top', horizontalalignment='left', \
         transform=ax.transAxes, color=myblue, size=16)
-    ax.text(0.05, 0.06, r'$\mathrm{H\alpha}$', verticalalignment='top', horizontalalignment='left', \
+    ax.text(0.02, 0.06, r'$\mathrm{H\alpha}$' + '               ' + r'$\mathrm{Velocity\ scale\, (km/s):[-602,+770]}$', \
+        verticalalignment='top', horizontalalignment='left', \
         transform=ax.transAxes, color=myred, size=16)
+
+    # Add text for galaxies and bridge
+    #r'$\mathrm{Taffy}$' + '-' + r'$\mathrm{N}$' + '\n' + r'$\mathrm{UGC\ 12915}$', \
+    ax.text(0.02, 0.96, 'Taffy-N' + '\n' + 'UGC 12915', \
+        verticalalignment='top', horizontalalignment='left', \
+        transform=ax.transAxes, color='k', fontproperties=f, size=20)
+    ax.text(0.67, 0.3, 'Taffy-S' + '\n' + 'UGC 12914', \
+        verticalalignment='top', horizontalalignment='left', \
+        transform=ax.transAxes, color='k', fontproperties=f, size=20)
+    ax.text(0.59, 0.85, 'Taffy Bridge', verticalalignment='top', horizontalalignment='left', \
+        transform=ax.transAxes, color='k', fontproperties=f, size=20)
 
     # ------------------------ PLOT THE REGIONS ------------------------ #
     reg_file = open(taffy_extdir + 'rawspectra_for_paperplot/regions_for_line_profiles.reg')
@@ -263,7 +301,7 @@ if __name__ == '__main__':
         ax.add_patch(region)
 
     # ------------------------ PLOT THE SPECTRA ------------------------ #
-    ax_n_1_blue, ax_n_1_red = plot_line_profiles(ax_n_1_blue, ax_n_1_red, north_majax_1_blue['wav'], north_majax_1_blue['flux'], north_majax_1_red['wav'], north_majax_1_red['flux'], 55,90, 40,175)
+    ax_n_1_blue, ax_n_1_red = plot_line_profiles(ax_n_1_blue, ax_n_1_red, north_majax_1_blue['wav'], north_majax_1_blue['flux'], north_majax_1_red['wav'], north_majax_1_red['flux'], 55,90, 40,180)
     ax_n_2_blue, ax_n_2_red = plot_line_profiles(ax_n_2_blue, ax_n_2_red, north_majax_2_blue['wav'], north_majax_2_blue['flux'], north_majax_2_red['wav'], north_majax_2_red['flux'], 40,80, 60,320)
     ax_n_3_blue, ax_n_3_red = plot_line_profiles(ax_n_3_blue, ax_n_3_red, north_majax_3_blue['wav'], north_majax_3_blue['flux'], north_majax_3_red['wav'], north_majax_3_red['flux'], 20,48, 20,235)
     ax_n_4_blue, ax_n_4_red = plot_line_profiles(ax_n_4_blue, ax_n_4_red, north_majax_4_blue['wav'], north_majax_4_blue['flux'], north_majax_4_red['wav'], north_majax_4_red['flux'], 15,50, 15,95)
@@ -300,34 +338,67 @@ if __name__ == '__main__':
     ax_br_3_red.set_xticklabels(ax_br_3_red.get_xticks().tolist(), size=10, rotation=20)
 
     # ------------------ Text for regionname ------------------- #
-    ax_n_1_red.text(0.0, 0.98, 'N1', verticalalignment='top', horizontalalignment='left', \
+    ax_n_1_red.text(0.05, 0.96, 'N1', verticalalignment='top', horizontalalignment='left', \
         transform=ax_n_1_red.transAxes, color='k', fontproperties=f, size=12)
-    ax_n_2_red.text(0.0, 0.98, 'N2', verticalalignment='top', horizontalalignment='left', \
+    ax_n_2_red.text(0.05, 0.96, 'N2', verticalalignment='top', horizontalalignment='left', \
         transform=ax_n_2_red.transAxes, color='k', fontproperties=f, size=12)
-    ax_n_3_red.text(0.0, 0.98, 'N3', verticalalignment='top', horizontalalignment='left', \
+    ax_n_3_red.text(0.05, 0.96, 'N3', verticalalignment='top', horizontalalignment='left', \
         transform=ax_n_3_red.transAxes, color='k', fontproperties=f, size=12)
-    ax_n_4_red.text(0.0, 0.98, 'N4', verticalalignment='top', horizontalalignment='left', \
+    ax_n_4_red.text(0.05, 0.96, 'N4', verticalalignment='top', horizontalalignment='left', \
         transform=ax_n_4_red.transAxes, color='k', fontproperties=f, size=12)
-    ax_n_5_red.text(0.0, 0.98, 'N5', verticalalignment='top', horizontalalignment='left', \
+    ax_n_5_red.text(0.05, 0.96, 'N5', verticalalignment='top', horizontalalignment='left', \
         transform=ax_n_5_red.transAxes, color='k', fontproperties=f, size=12)
 
-    ax_s_1_red.text(0.0, 0.98, 'S1', verticalalignment='top', horizontalalignment='left', \
+    ax_s_1_red.text(0.05, 0.96, 'S1', verticalalignment='top', horizontalalignment='left', \
        transform=ax_s_1_red.transAxes, color='k', fontproperties=f, size=12)
-    ax_s_2_red.text(0.0, 0.98, 'S2', verticalalignment='top', horizontalalignment='left', \
+    ax_s_2_red.text(0.05, 0.96, 'S2', verticalalignment='top', horizontalalignment='left', \
        transform=ax_s_2_red.transAxes, color='k', fontproperties=f, size=12)
-    ax_s_3_red.text(0.0, 0.98, 'S3', verticalalignment='top', horizontalalignment='left', \
+    ax_s_3_red.text(0.05, 0.96, 'S3', verticalalignment='top', horizontalalignment='left', \
        transform=ax_s_3_red.transAxes, color='k', fontproperties=f, size=12)
-    ax_s_4_red.text(0.0, 0.98, 'S4', verticalalignment='top', horizontalalignment='left', \
+    ax_s_4_red.text(0.05, 0.96, 'S4', verticalalignment='top', horizontalalignment='left', \
        transform=ax_s_4_red.transAxes, color='k', fontproperties=f, size=12)
-    ax_s_5_red.text(0.0, 0.98, 'S5', verticalalignment='top', horizontalalignment='left', \
+    ax_s_5_red.text(0.05, 0.96, 'S5', verticalalignment='top', horizontalalignment='left', \
         transform=ax_s_5_red.transAxes, color='k', fontproperties=f, size=12)
 
-    ax_br_1_blue.text(0.05, 0.98, 'B1', verticalalignment='top', horizontalalignment='left', \
+    ax_br_1_blue.text(0.05, 0.96, 'B1', verticalalignment='top', horizontalalignment='left', \
         transform=ax_br_1_blue.transAxes, color='k', fontproperties=f, size=12)
-    ax_br_2_blue.text(0.05, 0.98, 'B2', verticalalignment='top', horizontalalignment='left', \
+    ax_br_2_blue.text(0.05, 0.96, 'B2', verticalalignment='top', horizontalalignment='left', \
         transform=ax_br_2_blue.transAxes, color='k', fontproperties=f, size=12)
-    ax_br_3_blue.text(0.05, 0.98, 'B3', verticalalignment='top', horizontalalignment='left', \
+    ax_br_3_blue.text(0.05, 0.96, 'B3', verticalalignment='top', horizontalalignment='left', \
         transform=ax_br_3_blue.transAxes, color='k', fontproperties=f, size=12)
+
+    # Text for flux units
+    ax_n_1_blue.text(0.03, 1.04, r'$\times 10^{-18}\, \mathrm{erg\, s^{-1}\, cm^{-2}\, \AA^{-1}}$', \
+        verticalalignment='top', horizontalalignment='left', \
+        transform=ax_n_1_blue.transAxes, color='k', size=9)
+    ax_n_1_red.text(0.03, 1.04, r'$\times 10^{-18}\, \mathrm{erg\, s^{-1}\, cm^{-2}\, \AA^{-1}}$', \
+        verticalalignment='top', horizontalalignment='left', \
+        transform=ax_n_1_red.transAxes, color='k', size=9)
+
+    # Text for [NII] lines seen in some cases
+    ax_n_3_red.text(0.08, 0.42, r'$\mathrm{[NII]}$' + '\n' + r'$\lambda6548$', \
+        verticalalignment='top', horizontalalignment='left', \
+        transform=ax_n_3_red.transAxes, color='k', size=10)
+    ax_n_4_red.text(0.07, 0.51, r'$\mathrm{[NII]}$' + '\n' + r'$\lambda6548$', \
+        verticalalignment='top', horizontalalignment='left', \
+        transform=ax_n_4_red.transAxes, color='k', size=10)
+
+    ax_s_1_red.text(0.05, 0.39, r'$\mathrm{[NII]}$' + '\n' + r'$\lambda6548$', \
+        verticalalignment='top', horizontalalignment='left', \
+        transform=ax_s_1_red.transAxes, color='k', size=10)
+    ax_s_3_red.text(0.75, 0.52, r'$\mathrm{[NII]}$' + '\n' + r'$\lambda6583$', \
+        verticalalignment='top', horizontalalignment='left', \
+        transform=ax_s_3_red.transAxes, color='k', size=10)
+    ax_s_4_red.text(0.75, 0.73, r'$\mathrm{[NII]}$' + '\n' + r'$\lambda6583$', \
+        verticalalignment='top', horizontalalignment='left', \
+        transform=ax_s_4_red.transAxes, color='k', size=10)
+    ax_s_5_red.text(0.75, 0.77, r'$\mathrm{[NII]}$' + '\n' + r'$\lambda6583$', \
+        verticalalignment='top', horizontalalignment='left', \
+        transform=ax_s_5_red.transAxes, color='k', size=10)
+
+    ax_br_1_red.text(0.03, 0.39, r'$\mathrm{[NII]}$' + '\n' + r'$\lambda6548$', \
+        verticalalignment='top', horizontalalignment='left', \
+        transform=ax_br_1_red.transAxes, color='k', size=10)
 
     # Save figure
     fig.savefig(taffy_extdir + 'figures_stitched_cube/line_profile_plot.eps', dpi=300, bbox_inches='tight')
